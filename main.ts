9e63794e585f345d63f6ec3ba02d3049bf7ee685
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-20 10:34:40 -0400

    I changed world to MTHS

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program will say hello world to MTS
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
